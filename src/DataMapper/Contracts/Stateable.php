6a54579031723886213c8b21dc577cb34147ae54
dm <dmitrij.demidovich@gmail.com>
2020-09-15 00:05:56 +0300

    updated d2-hydrator

--- src/DataMapper/Contracts/Stateable.php
+++ src/DataMapper/Contracts/Stateable.php
@@ -4,5 +4,18 @@ namespace D2\DataMapper\Contracts;
 
 interface Stateable
 {
+    // /**
+    //  * Retrieve entity from state.
+    //  *
+    //  * @param string|array|object $state
+    //  */
+    // public static function fromState($state);
+
+    /**
+     * Export entity to persist.
+     *
+     * @return string|array
+     */
     public function toState();
+
 }
--- tests/Stubs/UserId.php
+++ tests/Stubs/UserId.php
@@ -2,9 +2,24 @@
 
 namespace Tests\Stubs;
 
-use D2\DataMapper\ValueObjects\Id;
+use D2\DataMapper\Contracts\Stateable;
 
-class UserId extends Id
+class UserId implements Stateable
 {
+    private int $value;
 
+    public function __construct(int $value)
+    {
+        $this->value = $value;
+    }
+
+    public function toState(): int
+    {
+        return $this->value;
+    }
+
+    public function value(): int
+    {
+        return $this->value;
+    }
 }
